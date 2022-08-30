ddd1d238675f77d3311f4816da1824ed4ea2a65b
Emmanuel Sahr Sewah <sewah2012@gmail.com>
2022-08-30 16:19:26 +0100

    project setup

--- grpc-client/src/main/java/com/grpc/user/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.grpc.user.repository;
-
-import com.grpc.user.document.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User, String> {
-
-}
